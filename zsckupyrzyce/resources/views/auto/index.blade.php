ce8354e7069b066c654acc6df194b998b869e7be
Umberto <horman0056@gmail.com>
2019-10-09 09:32:29 +0200

    zarys skryptu

--- zsckupyrzyce/resources/views/auto/index.blade.php
+++ zsckupyrzyce/resources/views/auto/index.blade.php
@@ -1,6 +1,34 @@
 @include('head')
 <h1>Oblicz poziom zanieczyszen auta</h1>
 <div class="panel">
-dfgdfgfdg
+    <script type="text/javascript"> 
+        var wynik;
+            function oblicz() {
+              var x = document.querySelector('#x').value;
+              var y = document.querySelector('#i').value;
+              var normalny = document.querySelector('#normalny').checked;
+              var ulgowy = document.querySelector('#ulgowy').checked;
+            if (x <= 0 || y <= 0){
+                document.querySelector(".wynik").innerHTML = "Podaj prawidłową odległość, ilość, lub wybierz rodzaj biletu!"}
+            else if (normalny == true ) {
+            document.querySelector(".wynik").innerHTML = x;
+            }
+            else if (ulgowy == true ) {
+            document.querySelector(".wynik").innerHTML = x;
+            }
+            }
+            </script>
+            <h1>Biletomat</h1>
+            Odległość od: <input type="number" id="x" name="pole"/>
+            
+            Odległość do: <input type="number" id="y" name="polle"/>
+            
+            <button id="normalny" name="add">+</button>
+            <button id="ulgowy" name="del">-</button>
+            
+            <!-- <input type="button" id="oblicz" value="PRZELICZ" onclick="oblicz()"/> -->
+                
+            <div class="wynik">
+                
 </div>
 {{route("kalkulator")}}
