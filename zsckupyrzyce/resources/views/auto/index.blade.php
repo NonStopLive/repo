7d02f46073985f8b5eec67b3abb4942631251dcc
Mateusz <fedorukmateusz@wp.pl>
2019-10-09 12:08:38 +0200

    edycja widoku

--- zsckupyrzyce/resources/views/auto/index.blade.php
+++ zsckupyrzyce/resources/views/auto/index.blade.php
@@ -19,12 +19,14 @@
             }
             </script>
             <h1>Biletomat</h1>
-            Odległość od: <input type="number" id="x" name="pole"/>
-            
+            <div class="input-group mb-3">
+                <div class="input-group-append">
+                <input type="text" class="form-control" placeholder="Podaj odległość">
+            </div>
             Odległość do: <input type="number" id="y" name="polle"/>
             
-            <button id="normalny" name="add">+</button>
-            <button id="ulgowy" name="del">-</button>
+            <button id="normalny" name="add" class="btn btn-outline-primary btn-rounded waves-effect" style="border-radius:50%;height:40px;width:40px;">+</button>
+            <button id="ulgowy" name="del"class="btn btn-outline-primary btn-rounded waves-effect" style="border-radius:50%;height:40px;width:40px;">-</button>
             
             <!-- <input type="button" id="oblicz" value="PRZELICZ" onclick="oblicz()"/> -->
                 
