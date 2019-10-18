c0a3874f3a69e9e9930b1cb0840f51a8c69bff74
Paweł Mazur <hamiamiam24@gmail.com>
2019-10-18 13:58:12 +0200

    somebody once told me

--- zsckupyrzyce/resources/views/head.blade.php
+++ zsckupyrzyce/resources/views/head.blade.php
@@ -10,6 +10,16 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <!-- Styles -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <style>
+        .content {
+                    height: 100vh;
+                    background: #000;
+                    color: #fff;
+                    text-align: center;
+                    display: table-cell;
+                    width: 100%;
+                    vertical-align: middle;
+                }
+        </style>
     </head>
     <body>
--- zsckupyrzyce/resources/views/welcome.blade.php
+++ zsckupyrzyce/resources/views/welcome.blade.php
@@ -1,107 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-                background-color: black;
-                font-size: 40px;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 80px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-            @keyframes flickerAnimation {
-                0%   { opacity:1; }
-                50%  { opacity:0; }
-                100% { opacity:1; }
-            }
-            @-o-keyframes flickerAnimation{
-                0%   { opacity:1; }
-                50%  { opacity:0; }
-                100% { opacity:1; }
-            }
-            @-moz-keyframes flickerAnimation{
-                0%   { opacity:1; }
-                50%  { opacity:0; }
-                100% { opacity:1; }
-            }
-            @-webkit-keyframes flickerAnimation{
-                0%   { opacity:1; }
-                50%  { opacity:0; }
-                100% { opacity:1; }
-            }
-            .animate-flicker {
-            -webkit-animation: flickerAnimation 1s infinite;
-            -moz-animation: flickerAnimation 1s infinite;
-            -o-animation: flickerAnimation 1s infinite;
-            animation: flickerAnimation 1s infinite;
-            }
-            #welcome {
-            
-display: block;
- 
-height: 100vh;
-width: 100%;
-margin: 33vh 0;
-
-            }
-        </style>
-    </head>
-    <body onkeypress="myFunction(event)">
-        <div id="welcome" class="flex-center position-ref full-height">
+@include('head')
+    <body id="welcome" onkeypress="myFunction(event)">
+        <div id="home" class="flex-center position-ref full-height" style="display: table;
+        width: 100%;">
             {{-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -116,7 +17,7 @@ margin: 33vh 0;
                 </div>
             @endif --}}
 
-            <div class="content"  style="opacity:1;transition:0.5s all ease-in-out;">
+            <div class="content"  style="opacity:1;transition:0.5s all ease-in-out;display: table-cell;">
                 <div class="title m-b-md">
                     Projekt ZSCKU PYRZYCE
                 </div>
@@ -126,7 +27,7 @@ margin: 33vh 0;
                   
                 </div>
             </div>
-            <div class="second" style="opacity:0;transition:0.5s all ease-in-out;">
+            <div class="second" style="opacity:0;display: none;transition:0.5s all ease-in-out;">
               @include('auto.index')
             </div>
             <script>    
@@ -136,6 +37,8 @@ margin: 33vh 0;
                         if(parseInt(x) == 13)  {
                            document.querySelector(".content").style.opacity = '0';
                            document.querySelector(".second").style.opacity = '1';
+                           document.querySelector(".second").style.display = '';
+                           document.querySelector(".content").style.display = 'none';
                         }
                     }
                 </script>
